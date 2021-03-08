19e9efee76fbcf47dafbbd058d109272e53933b2
José Lastra <lastrafda@gmail.com>
2021-03-07 23:52:51 -0300

    feat: add prepend function

--- 04/lists.js
+++ 04/lists.js
@@ -16,11 +16,15 @@ function listToArray(list) {
   return output;
 }
 
+function prepend(value, rest) {
+  return { value, rest };
+}
+
 console.log(arrayToList([10, 20, 30]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 // console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
